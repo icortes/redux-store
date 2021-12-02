373809d6f99c17c3834dfa3a2b2dcf191dd93b4c
isa <5445946+icortes@users.noreply.github.com>
2021-12-01 17:56:06 -0800

    updated Detail.js to use react-redux

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -13,9 +13,12 @@ import {
 import { QUERY_PRODUCTS } from '../utils/queries';
 import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
+import { useSelector, useDispatch } from 'react-redux';
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
+  
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
